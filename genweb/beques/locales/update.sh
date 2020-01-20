633020f14e962c74533747e56b95a05e6551d174
alberto.duran <alberto.duran@upcnet.es>
2020-01-20 11:03:34 +0100

    Locales added

---
+++ genweb/beques/locales/update.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+domain=genweb.beques
+
+export PATH=../../../../../bin
+
+i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
